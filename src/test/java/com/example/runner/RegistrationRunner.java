40672b75d0e1e4b4890886809f946f3675a8966f
Nitin Jain <nitin_jain3@epam.com>
2024-06-26 16:10:12 +0530

    adding BDD project

--- src/test/java/com/example/runner/RegistrationRunner.java
+++ src/test/java/com/example/runner/RegistrationRunner.java
@@ -8,11 +8,11 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features={"src/test/resources/SigninAndSignOut.feature"},
+@CucumberOptions(features="src/test/resources/",
 glue="com.example.stepdefinations",
 monochrome=true,
 plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber-reports.json"},
-tags="Smoke" )
+tags="@Smoke")
 	
 
 
